99d616b8ccef259f6e1f6cf482b2500b8a412d9b
Volodymyr Sychov <86024945+vovanych320@users.noreply.github.com>
2023-05-03 22:01:26 +0300

    Eslint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+  res.send('Hello world!');
+});
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
